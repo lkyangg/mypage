8d75d59f00c7c016cde2c817275d61d043d49b0a
lkyangg <108116872+lkyangg@users.noreply.github.com>
2024-08-21 19:11:30 +0800

    Add files via upload

--- home.py
+++ home.py
@@ -3,12 +3,16 @@ import streamlit.components.v1 as components
 
 # 设置为宽页面
 st.set_page_config(layout="wide", page_title="lkyangg's page")
-st.title("lkyangg's page")
 
+st.title("lkyangg's page")
+st.write("您好，欢迎来到此页面，以下是我的几个可视化效果图展示：")
 
 with open('./html/wherever.html', encoding='utf-8') as f:
-    components.html(f.read(), height=1800)
+    components.html(f.read(), height=1200)
 
-st.write('---')
-with open('./html/gdkfq_0.html', encoding='utf-8') as p:
+with open('./html/gdkfq_map.html', encoding='utf-8') as p:
+    components.html(p.read(), height=800)
+
+# st.write('---')
+with open('./html/stars.html', encoding='utf-8') as p:
     components.html(p.read(), height=800)
